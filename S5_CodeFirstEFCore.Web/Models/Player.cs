359db91d5b1734e8e755889ad5b85427e932c88b
Paolo Maldonado <jorpal_10_20@outlook.com>
2021-05-20 20:30:10 -0500

    Add project files.

---
+++ S5_CodeFirstEFCore.Web/Models/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S5_CodeFirstEFCore.Web.Models
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(200)")]
+        [Display(Name = "Nombre del futbolista")]
+        public string FullName { get; set; }
+
+        [Column(TypeName = "int")]
+        [Display(Name = "Numero de camiseta")]
+        public int Dorsal { get; set; }
+
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fec. de Nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public int SoccerPositionId { get; set; }
+
+        public virtual SoccerPosition SoccerPosition { get; set; }
+
+    }
+}
